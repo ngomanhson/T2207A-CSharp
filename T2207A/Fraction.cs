f23a07ddc8d6498e254a1af964495ce8a0b06eb4
NgoManhSon <ngomanhson2004txpt@gmail.com>
2023-04-13 22:33:42 +0700

    Nộp bìa tập 13/4/23

---
+++ T2207A/Fraction.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T2207A
+{
+    public class Fraction
+    {
+        public int tuSo;
+        public int mauSo;
+
+        public Fraction(int tuSo, int mauSo)
+        {
+            this.tuSo = tuSo;
+            this.mauSo = mauSo;
+        }
+
+        public Fraction()
+        {
+
+        }
+
+        //Nhap phan so
+        public void NhapPhanSo()
+        {
+            do
+            {
+                Console.WriteLine("Nhap tu so: ");
+                string nts = Console.ReadLine();
+                tuSo = Convert.ToInt32(nts);
+                //Console.WriteLine("Phan so vua nhap: " + t);
+
+                Console.WriteLine("Nhap mau so: ");
+                string nms = Console.ReadLine();
+                mauSo = Convert.ToInt32(nms);
+                //Console.WriteLine("Phan so vua nhap: " + m);
+            }
+            while (mauSo == 0);
+        }
+
+        //In phan so
+        public void InPhanSo()
+        {
+            Console.WriteLine(tuSo +"/"+ mauSo);
+        }
+
+        //Uoc chung lon nhat
+        private int TimUCLN(int a, int b)
+        {
+            {
+                a = Math.Abs(a);
+                b = Math.Abs(b);
+
+                while (a != b)
+                {
+                    if (a > b)
+                        a -= b;
+                    else
+                        b -= a;
+                }
+                return a;
+            }
+        }
+
+        //Rut gon phan so
+        public void RutGon()
+        {
+            int ucln = TimUCLN(tuSo, mauSo);
+            tuSo /= ucln;
+            mauSo /= ucln;
+        }
+
+        //Nghich dao phan so
+        public void NghichDao()
+        {
+            int nghich = tuSo;
+            tuSo = mauSo;
+            mauSo = nghich;
+        }
+
+        //Cong 2 phan so
+        public static Fraction Add(Fraction ps1, Fraction ps2)
+        {
+            Fraction ps = new Fraction();
+            ps.tuSo = ps1.tuSo * ps2.mauSo + ps2.tuSo * ps1.mauSo;
+            ps.mauSo = ps1.mauSo * ps2.mauSo;
+            ps.RutGon();
+            return ps;
+        }
+
+        // Tru 2 phan so
+        public static Fraction Sub(Fraction ps1, Fraction ps2)
+        {
+            Fraction ps = new Fraction();
+            ps.tuSo = ps1.tuSo * ps2.mauSo - ps2.tuSo * ps1.mauSo;
+            ps.mauSo = ps1.mauSo * ps2.mauSo;
+            ps.RutGon();
+            return ps;
+        }
+
+        //Nhan 2 phan so
+        public static Fraction Mul(Fraction ps1, Fraction ps2)
+        {
+            Fraction ps = new Fraction();
+            ps.tuSo = ps1.tuSo * ps2.tuSo;
+            ps.mauSo = ps1.mauSo * ps2.mauSo;
+            ps.RutGon();
+            return ps;
+        }
+
+        //Chia 2 phan so
+        public static Fraction Div(Fraction ps1, Fraction ps2)
+        {
+            Fraction ps = new Fraction();
+            ps.tuSo = ps1.tuSo * ps2.mauSo;
+            ps.mauSo = ps1.mauSo * ps2.tuSo;
+            ps.RutGon();
+            return ps;
+        }
+
+        public String ToString()
+        {
+            return tuSo + "/" + mauSo;
+        }
+    }
+    
+}
---
+++ T2207A/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using System.Collections.Generic;
+using T2207A;
+
+public class Program
+{
+    public static void Main(String[] args)
+    {
+        //Phan so
+        Fraction ps1 = new Fraction();
+        ps1.NhapPhanSo();
+        Console.Write("Phan so thu nhat la: ");
+        ps1.InPhanSo();
+
+        Fraction ps2 = new Fraction();
+        ps2.NhapPhanSo();
+        Console.Write("Phan so thu hai la: ");
+        ps2.InPhanSo();
+
+        Console.WriteLine("==========================");
+
+        Console.Write("Tong hai phan so la: ");
+        Fraction ps3 = Fraction.Add(ps1, ps2);
+        ps3.InPhanSo();
+
+        Console.Write("Hieu hai phan so la: ");
+        Fraction ps4 = Fraction.Sub(ps1, ps2);
+        ps4.InPhanSo();
+
+        Console.Write("Tich hai phan so la: ");
+        Fraction ps5 = Fraction.Mul(ps1, ps2);
+        ps5.InPhanSo();
+
+        Console.Write("Thuong hai phan so la: ");
+        Fraction ps6 = Fraction.Div(ps1, ps2);
+        ps6.InPhanSo();
+    }
+}
+
